b8010034100d854cf3102dbeb40906a653e1ac74
ManueleVeggi <manuele.veggi@studio.unibo.it>
2026-05-06 11:39:00 +0200

    prepare 2

--- js/project-run/subject-manager.js
+++ js/project-run/subject-manager.js
@@ -1,15 +1,5 @@
-window.addEventListener('DOMContentLoaded', async function() {
-
-    await updateStorageObjects();
-
-    initializeSubjectScene();
-
-});
 
 function initializeSubjectScene() {}
-
-
-
 
 
 // NEED AN EASY WAY TO CONNECT VR TO URL
